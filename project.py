c873345d83590337e640290f62455d435d064a72
Given Ncube <givenyslim12@gmail.com>
2020-12-05 09:38:47 +0000

    basic cli is now working

--- project.py
+++ project.py
@@ -12,10 +12,11 @@ from sklearn.feature_extraction.text import CountVectorizer
 import string
 
 def combine_sim(p, all):
+    print('combining...')
     combined = p
     for answer in all:
         for para in answer:
-            if p == para.text:
+            if p == para.text or len(para.text.strip()) < 70:
                 continue
             if similar([p, para.text]):
                 combined += para.text
@@ -25,18 +26,19 @@ def combine_sim(p, all):
 
 
 def similar(documents):
+    print('cleaning...')
     cleaned = list(map(clean_text, documents))
+    try:
+        vectorizer = CountVectorizer().fit_transform(cleaned)
+        vectors = vectorizer.toarray()
+        threshold = cosine_sim_vectors(vectors[0], vectors[1])
+        if threshold >= 0.5:
+            return True
 
-    vectorizer = CountVectorizer().fit_transform(cleaned)
-    vectors = vectorizer.toarray()
-
-    threshold = cosine_sim_vectors(vectors[0], vectors[1])
-
-    if threshold >= 0.5:
-        return True
-    else:
+    except Exception as e:
         return False
 
+    return False
 
 def cosine_sim_vectors(vec1, vec2):
     vec1 = vec1.reshape(1, -1)
@@ -56,6 +58,7 @@ def clean_text(text):
 
 
 def summarize(article_text):
+    print('summarizing...')
     article_text = re.sub(r'\[[0-9]*\]', ' ', article_text)
     article_text = re.sub(r'\s+', ' ', article_text)
 
@@ -90,7 +93,7 @@ def summarize(article_text):
                     else:
                         sentence_scores[sent] += word_frequencies[word]
 
-    summary_sentences = heapq.nlargest(1, sentence_scores, key=sentence_scores.get)
+    summary_sentences = heapq.nlargest(5, sentence_scores, key=sentence_scores.get)
     summary = ' '.join(summary_sentences)
 
     return summary
@@ -104,14 +107,16 @@ links = []
 article_text = ""
 
 
-query = "hackernoon How To Scrape Google With Python"
+query = "The role of technology in the retail industry"
 query = query.replace(' ', '+')
 URL = f"https://google.com/search?q={query}"
 USER_AGENT =  "Mozilla/5.0 (Linux; Android 7.0; SM-G930V Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.125 Mobile Safari/537.36"
 headers = {"user-agent" : USER_AGENT}
+print('googling...')
 resp = requests.get(URL, headers=headers)
 
 if resp.status_code == 200:
+    print('googled...')
     soup = bs.BeautifulSoup(resp.content, "lxml")
     a = soup.find_all('a')
     for i in a:
@@ -130,7 +135,9 @@ if resp.status_code == 200:
 
 
 answers = []
+limit = 7
 for link in links:
+    print('following links... ' + link)
     try:
         scraped_data = urllib.request.urlopen(link)
     except Exception as e:
@@ -142,16 +149,22 @@ for link in links:
     paragraphs = parsed_article.find_all('p')
 
     answers.append(paragraphs)
+    limit += 1
 
 alike = []
 
 for answer in answers:
+    print('grouping...')
     for p in answer:
-        alike.append(combine_sim(p.text, answers))
+        combined = combine_sim(p.text, answers)
+        print('combined...')
+        print(combined)
+        if len(combined.strip()) < 7:
+            continue
+        summary = summarize(combined)
+        alike.append(summary)
 
 article_text = ""
 
 for p in alike:
-    article_text += p
-
-print(summarize(article_text))
+    print(p)
